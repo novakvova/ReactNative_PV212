11435c17a35bcd1dd00295a01094485341195b7a
novakvova <novakvova@gmail>
2025-04-02 19:14:18 +0300

    Is good working app

--- components/ui/IconSymbol.tsx
+++ components/ui/IconSymbol.tsx
@@ -13,6 +13,8 @@ const MAPPING = {
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
+  'person.crop.circle': 'person', // Login
+  'person.badge.plus': 'person-add', // Signup
 } as Partial<
   Record<
     import('expo-symbols').SymbolViewProps['name'],
