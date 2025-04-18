95b544bbb1f43459c47715d532257f859cfacf03
novakvova <novakvova@gmail>
2025-04-18 18:54:00 +0300

    Is good working Register

--- WebCatApi/WebCatApi/Program.cs
+++ WebCatApi/WebCatApi/Program.cs
@@ -24,6 +24,8 @@ builder.Services.AddIdentity<UserEntity, RoleEntity>(options =>
 builder.Services.AddScoped<IImageService, ImageService>();
 builder.Services.AddScoped<IJwtTokenService, JwtTokenService>();
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 // builder.Services.AddOpenApi();
